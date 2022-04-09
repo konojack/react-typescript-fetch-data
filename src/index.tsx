7b04ec127a272e3b5cb623c942b7b89c97cb1a49
konojack <konojack@gmail.com>
2022-04-09 19:14:01 +0200

    Create redux template

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,15 @@
-import { useState } from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-const App: React.FC<{ color?: string }> = ({ color }): JSX.Element => {
-  const [counter, setCounter] = useState<number>(0);
+import { App } from './components/App';
+import { reducers } from './reducers';
 
-  const onIncrement = (): void => {
-    setCounter((prevState: number) => prevState + 1);
-  };
-
-  const onDecrement = (): void => {
-    setCounter((prevState: number) => prevState - 1);
-  };
-
-  return (
-    <div>
-      <button onClick={onIncrement}>Increment</button>
-      <button onClick={onDecrement}>Decrement</button>
-      {counter}
-    </div>
-  );
-};
-
-ReactDOM.render(<App color="red" />, document.querySelector('#root'));
+const store = createStore(reducers, applyMiddleware(thunk));
+ReactDOM.render(
+  <Provider store={store}>
+    <App></App>
+  </Provider>,
+  document.querySelector('#root')
+);
