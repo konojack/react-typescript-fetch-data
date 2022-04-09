83119f2a122d5f648120a4d695419e04cc58b524
konojack <konojack@gmail.com>
2022-04-09 18:56:48 +0200

    Increment, decrement

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App: React.FC<{ color?: string }> = ({ color }) => {
-  return <div>{`Hi There ${color}!`}</div>;
+  const [counter, setCounter] = useState<number>(0);
+
+  const onIncrement = (): void => {
+    setCounter((prevState: number) => prevState + 1);
+  };
+
+  const onDecrement = (): void => {
+    setCounter((prevState: number) => prevState - 1);
+  };
+
+  return (
+    <div>
+      <button onClick={onIncrement}>Increment</button>
+      <button onClick={onDecrement}>Decrement</button>
+      {counter}
+    </div>
+  );
 };
 
 ReactDOM.render(<App color="red" />, document.querySelector('#root'));
