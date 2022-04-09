d052d316759fa195395bc6e877c2f8e2def8b224
konojack <konojack@gmail.com>
2022-04-10 01:20:00 +0200

    fetching data with button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,26 @@
-export const App = () => {
-  return <div>Hello!</div>;
+import { useDispatch, useSelector } from 'react-redux';
+import { StoreState } from '../reducers';
+import { fetchTodos } from '../actions';
+import { Todo } from '../actions';
+
+export const App = (): JSX.Element => {
+  const todos = useSelector((state: StoreState) => state.todos);
+  const dispatch = useDispatch();
+
+  const onButtonClick = (): void => {
+    dispatch(fetchTodos());
+  };
+
+  const renderList = (): JSX.Element[] => {
+    return todos.map((todo: Todo) => {
+      return <li key={todo.id}>{todo.title}</li>;
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={onButtonClick}>Fetch</button>
+      {renderList()}
+    </div>
+  );
 };
