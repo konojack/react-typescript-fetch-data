ff38794b4594b311acbaf054b02e05fb5702be26
konojack <konojack@gmail.com>
2022-04-09 18:16:49 +0200

    First type in App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return <div>Hi There!</div>;
+const App: React.FC<{ color?: string }> = ({ color }) => {
+  return <div>{`Hi There ${color}!`}</div>;
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App color="red" />, document.querySelector('#root'));
